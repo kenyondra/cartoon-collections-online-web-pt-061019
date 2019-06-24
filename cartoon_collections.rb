9ab538c6af7601bbb7844156d25ca8dd6c30011c
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-24 04:51:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 long_planeteer_calls(["puff", "go", "two"])
 
 def find_the_cheese(array)
-  array.find { |i| 
+  array.find { |i| i == 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
